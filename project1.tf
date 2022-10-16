9ee3097fcb63d92c3eed13412ce64ef6144525a0
Cosme Foko <cosme.kuatefoko@gmail.com>
2022-10-16 18:51:04 -0500

    new resource

--- project1.tf
+++ project1.tf
@@ -1,4 +1,15 @@
 resource "aws_instance" "web" {
-  ami           = "ami-0671aa4aca16d71fb"
+  ami           = "ami-08c40ec9ead489470"
   instance_type = "t2.micro"
+}
+resource "aws_s3_bucket" "11222onebucket" {
+   bucket = "testing-s3-with-terraform"
+   acl = "private"
+   versioning {
+      enabled = true
+   }
+   tags = {
+     Name = "Bucket1"
+     Environment = "Test"
+   }
 }
