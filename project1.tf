2d7e1abe6cdeb23348c3501c89451a3354e15a90
Cosme Foko <cosme.kuatefoko@gmail.com>
2022-10-16 18:54:28 -0500

    new correction

--- project1.tf
+++ project1.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
   ami           = "ami-08c40ec9ead489470"
   instance_type = "t2.micro"
 }
-resource "aws_s3_bucket" "11222onebucket" {
+resource "aws_s3_bucket" "a11222onebucket" {
    bucket = "testing-s3-with-terraform"
    acl = "private"
    versioning {
